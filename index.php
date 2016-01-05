ff71bd6b9e9e9b4c1b9e4a7a35d45e0f6a965231
Riverbell <emba93@live.se>
2016-01-05 14:52:38 +0100

    Första committen

---
+++ index.php
@@ -0,0 +1,144 @@
+<?php include 'prefix.php';?>
+<?php
+        session_start();
+?>
+<?xml version="1.0" ?>
+<!DOCTYPE bookingSystem [
+    <!ENTITY Aring "&#197;">
+    <!ENTITY aring "&#229;">
+    <!ENTITY Auml "&#196;">
+    <!ENTITY auml "&#228;">
+    <!ENTITY Ouml "&#214;">
+    <!ENTITY ouml "&#246;">
+]>
+
+<?php
+    //http://xml.csc.kth.se/~emmabac/DM2517/project/start.php
+    // Connect using host, username, password and databasename
+    $link = mysqli_connect('localhost', 'emmabac', 'emmabac-xmlpub13', 'emmabac');
+
+    // Check connection
+    if (mysqli_connect_errno()) {
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        exit();
+    }
+
+
+// EVENTS ========================================================
+    $query = "SELECT event_id, event_name, total_tickets, available_tickets, venue, startdate, starttime, user_email, category_id, user_name, category_name
+            FROM events NATURAL JOIN users
+            NATURAL JOIN categories
+            ORDER BY startdate, starttime";
+
+    // Execute the query
+    if (($result = mysqli_query($link, $query)) === false) {
+       printf("Query failed: %s<br />\n%s", $query, mysqli_error($link));
+       exit();
+    }
+
+    $event_tag = '';
+    while ($line = $result->fetch_object()) {
+        // Store results from each row in variables
+        $event_id = $line->event_id;
+        $event_name = $line->event_name;
+        $total_tickets = $line->total_tickets;
+        $available_tickets = $line->available_tickets;
+        $venue = $line->venue;
+        $startdate = $line->startdate;
+        $starttime = $line->starttime;
+        $creator = $line->user_name;
+        $category = $line->category_name;
+        $creator_id = $line->user_email;
+    
+        //if there is no user logged in
+        if(!isset($_SESSION['name'])) {
+            $event_tag .= "<event id='$event_id' edit='no' book='no'>
+                <name>$event_name</name>
+                <startdate>$startdate</startdate>
+                <starttime>$starttime</starttime>
+                <venue>$venue</venue>
+                <creator>$creator</creator>
+                <category>$category</category>
+                <total_tickets>$total_tickets</total_tickets>
+                <available_tickets>$available_tickets</available_tickets>
+            </event>";
+        } else {
+            //if the logged in user is the creator of this event or admin
+            if ($creator_id==$_SESSION['name'] || $_SESSION['user_type'] == 'admin') {
+                $event_tag .= "<event id='$event_id' edit='ok' book='no'>
+                    <name>$event_name</name>
+                    <startdate>$startdate</startdate>
+                    <starttime>$starttime</starttime>
+                    <venue>$venue</venue>
+                    <creator>$creator</creator>
+                    <category>$category</category>
+                    <total_tickets>$total_tickets</total_tickets>
+                    <available_tickets>$available_tickets</available_tickets>
+                </event>";
+            } elseif ( $_SESSION['user_type'] == 'user' ) {
+                $event_tag .= "<event id='$event_id' edit='no' book='ok'>
+                <name>$event_name</name>
+                <startdate>$startdate</startdate>
+                <starttime>$starttime</starttime>
+                <venue>$venue</venue>
+                <creator>$creator</creator>
+                <category>$category</category>
+                <total_tickets>$total_tickets</total_tickets>
+                <available_tickets>$available_tickets</available_tickets>
+            </event>";
+            } elseif ( $_SESSION['user_type'] == 'creator' ) {
+                $event_tag .= "<event id='$event_id' edit='no' book='no'>
+                <name>$event_name</name>
+                <startdate>$startdate</startdate>
+                <starttime>$starttime</starttime>
+                <venue>$venue</venue>
+                <creator>$creator</creator>
+                <category>$category</category>
+                <total_tickets>$total_tickets</total_tickets>
+                <available_tickets>$available_tickets</available_tickets>
+            </event>";
+            }
+    } 
+    } //end while
+
+    // Free result and just in case encode result to utf8 before returning
+    mysqli_free_result($result);
+
+    if(!isset($_SESSION['name'])) {
+        $menuItem = "<menuItem link='login.php'>Logga in</menuItem>";
+    } else {
+        $menuItem = "<menuItem link='logout.php'>Logga ut</menuItem>";
+
+    }
+        
+?>
+
+<bookingSystem>
+<menu>
+    <menuItem link="index.php">Startsida</menuItem>
+    <?php
+        if(isset($_SESSION['name'])) {
+            if($_SESSION['user_type'] == 'admin' || $_SESSION['user_type'] == 'creator') {
+                print utf8_encode("<menuItem link='create_event.php'>Skapa event</menuItem>");
+            }
+        }
+    ?>
+    <menuItem link="profile.php">Profil</menuItem>
+    <?php 
+        print utf8_encode($menuItem);
+    ?>
+</menu>
+<title>Coola bokningar!</title>
+<events>
+    <?php
+        print utf8_encode($event_tag);
+    ?>
+</events>
+
+
+</bookingSystem>
+
+<?php include 'postfix.php';?>
+
+
+
---
+++ index_xslt.xsl
@@ -0,0 +1,102 @@
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                xmlns:rss="http://purl.org/rss/1.0/"
+                xmlns:dc="http://purl.org/dc/elements/1.1/"
+                xmlns:syn="http://purl.org/rss/1.0/modules/syndication/"
+                xmlns="http://www.w3.org/1999/xhtml"
+                version="1.0">
+<xsl:output indent="yes"/> 
+
+<xsl:template match="bookingSystem">
+	<html>
+		<head>
+			<title><xsl:value-of select="title"/></title>
+			<link rel="stylesheet" type="text/css" href="/~emmabac/DM2517/project/style.css"/>
+		</head>
+		<body>
+			<h1><xsl:value-of select="title"/></h1>
+			<xsl:apply-templates/>
+
+		</body>
+	</html>
+</xsl:template>
+
+<xsl:template match="menu">
+	<div class="menu">
+		<h2>Meny</h2>
+		<ul>
+			<xsl:apply-templates/>
+		</ul>
+	</div>
+</xsl:template>
+
+<xsl:template match="menuItem">
+	<li>
+		<a href="{@link}"><xsl:apply-templates/></a>
+	</li>
+</xsl:template>
+
+<xsl:template match="event">
+	<xsl:variable name="event_id" select="@id"/>
+	<div class="container">
+		<h2>
+			<xsl:value-of select="name"/>
+		</h2>
+		<ul>
+			<xsl:apply-templates/>
+		</ul>
+		<!-- only show edit button if the logged in user is the creator -->
+		<xsl:if test="@edit = 'ok'">
+			<form action="update_event.php" method="post">
+				<input type="hidden" value="{$event_id}" name="event_id"/>
+				<input type="submit" value="Ändra"/>
+			</form>
+		</xsl:if>
+
+		<!-- only show book button if the logged in user is a user -->
+		<xsl:if test="@book = 'ok'">
+			<form action="book_event.php" method="post">
+				<input type="hidden" value="{$event_id}" name="event_id"/>
+				<input type="submit" value="Boka"/>
+			</form>
+		</xsl:if>
+	</div>
+</xsl:template>
+
+<xsl:template match="title"/>
+<xsl:template match="event/name"/>
+<xsl:template match="event/total_tickets"/>
+
+<xsl:template match="event/available_tickets">
+	<li>
+		Lediga platser: <xsl:apply-templates/> av <xsl:value-of select="../total_tickets"/>
+	</li>
+</xsl:template>
+
+<xsl:template match="event/venue">
+	<li>
+		Arena: <xsl:apply-templates/>
+	</li>
+</xsl:template>
+
+<xsl:template match="event/creator">
+	<li>
+		Arrangör: <xsl:apply-templates/>
+	</li>
+</xsl:template>
+
+<xsl:template match="event/category">
+	<li>
+		Kategori: <xsl:apply-templates/>
+	</li>
+</xsl:template>
+
+<xsl:template match="event/startdate">
+	<li>
+		Datum: <xsl:apply-templates/> kl <xsl:value-of select="../starttime"/>
+	</li>
+</xsl:template>
+
+<xsl:template match="event/starttime"/>
+
+</xsl:stylesheet>
